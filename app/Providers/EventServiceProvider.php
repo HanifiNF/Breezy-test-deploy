c259589ecd48c0bb988ad81afe3521b21d5467ea
Hanifi Abrar Setiawan <hanifisetiawan@gmail.com>
2025-12-30 12:46:33 +0800

    initial commit

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
+use App\Events\LoginHistory;
+use App\Listeners\storeUserLoginHistory;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        Registered::class => [
+            SendEmailVerificationNotification::class,
+        ],
+        LoginHistory::class => [
+            StoreUserLoginHistory::class,
+        ],
+        Event1::class => [
+            Listener1::class,
+            Listener2::class
+        ],
+        Event2::class => [
+            Listener5::class,
+            Listener7::class
+        ],
+        Event3::class => [
+            Listener4::class,
+            Listener7::class,
+            Listener9::class
+        ]
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
